b9e7cac5a87058e5f0b9ea02283865024370c60a
jhnl18 <jhunelpenaflorida@gmail.com>
2025-06-03 20:11:54 +0800

    Initial commit

---
+++ src/main/java/com/apps/logbook/Logbook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.apps.logbook;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Logbook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
